6d814f5e5221f3b603e17e12b042242b4b576f2a
Iwata <iratqq@gmail.com>
2009-10-02 15:40:24 +0900

    move poll to httpd.cxx.

--- utils.h
+++ utils.h
@@ -130,38 +130,4 @@ void set_priv(const char *, const char *, const char *);
 
 }
 
-#if !defined(HAVE_POLL) && !defined(HAVE_POLL_H)
-
-namespace std {
-
-typedef struct pollfd {
-	int 	fd;
-	short	events;
-	short	revents;
-} pollfd_t;
-
-typedef unsigned int	nfds_t;
-
-#define	POLLIN		0x0001
-#define	POLLOUT		0x0004
-#define	POLLERR		0x0008
-#if 0
-/* the following are currently not implemented */
-#define	POLLPRI		0x0002
-#define	POLLHUP		0x0010
-#define	POLLNVAL	0x0020
-#define	POLLRDNORM	0x0040
-#define POLLNORM	POLLRDNORM
-#define POLLWRNORM      POLLOUT
-#define	POLLRDBAND	0x0080
-#define	POLLWRBAND	0x0100
-#endif
-
-#define INFTIM		(-1)	/* not standard */
-
-int   poll(struct pollfd *, nfds_t, int);
-
-}
-#endif /* !HAVE_POLL_H */
-
 #endif /* _UTILS_H_ */
