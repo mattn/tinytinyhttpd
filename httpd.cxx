ee9574bea6bb14f6111e4099e73de038c68af570
Shinsuke Matsui <poppen.jp@gmail.com>
2009-11-02 17:39:26 +0900

    fixed bug with return value of sendfile() on FreeBSD.

--- httpd.cxx
+++ httpd.cxx
@@ -1734,7 +1734,7 @@ request_done:
 #if defined LINUX_SENDFILE_API
 			sent = sendfile(msgsock, fileno(res_info->read), NULL, total);
 #elif defined FREEBSD_SENDFILE_API
-			sent = sendfile(msgsock, fileno(res_info->read), NULL, total, NULL, NULL, 0);
+			if (sendfile(msgsock, fileno(res_info->read), NULL, total, NULL, NULL, 0) == 0) sent = total;
 #elif defined _WIN32
 			if (!res_info->process && lpfnTransmitFile && lpfnTransmitFile(
 				msgsock,
